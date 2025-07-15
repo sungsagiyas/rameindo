32efda073e9feec4c52949fe3fecd70d42011731
sungsagiyas <sungsagiyas@gmail.com>
2025-07-15 14:26:02 +0700

    Delete src directory

--- src/utils/site.ts
+++
@@ -1,4 +0,0 @@
-export const nama = 'Rame Indo';
-export const desk = 'Video Bokep Indo Bocil Ngentot Jilbab Smp Mama Rame Viral Colmek Sma Cindo Tante China';
-export const url = 'https://rameindo.pages.dev';
-export const terbit = '2025-01-27T00:01:03Z';
