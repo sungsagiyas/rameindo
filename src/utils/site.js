cac137304fa8c9ed34cdbf20f728a84e0459cda2
sungsagiyas <sungsagiyas@gmail.com>
2025-07-15 14:27:46 +0700

    Add files via upload

---
+++ src/utils/site.js
@@ -0,0 +1,10 @@
+export const nama = 'Rame Indo';
+export const desk = 'Video Bokep Indo Bocil Ngentot Jilbab Smp Mama Rame Viral Colmek Sma Cindo Tante China';
+export const url = 'https://rameindo.pages.dev';
+export const terbit = '2025-01-27T00:01:03Z';
+
+
+export const google = 'n88cnhFb4iG2kl16bHF0L7IsOnbYklE0JCu8DHT-x5s';
+export const yandex = 'b573f1f485cb9029'
+
+export const index = '3e675326-3e7e-42f6-b8e8-d29065704dcf';
